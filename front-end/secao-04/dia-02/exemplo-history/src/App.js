7101b30c05fe90f4abb01ffa52435d3f5cf8955f
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-03-18 18:55:24 -0300

    tiltei com o npm start n funcionando, fds.

--- front-end/secao-04/dia-02/exemplo-history/src/App.js
+++ front-end/secao-04/dia-02/exemplo-history/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Welcome from './pages/Welcome';
+import Home from './pages/Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/Welcome" component={Welcome} />
+        </Switch>
+      </BrowserRouter>
+
+    )
+  }
 }
 
 export default App;
