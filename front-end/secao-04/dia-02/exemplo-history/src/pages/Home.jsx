c840875f1d81240d1524254efc3bab6fa15e47b8
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-03-19 16:51:32 -0300

    fiz botao pra ir e voltar de pagina

--- front-end/secao-04/dia-02/exemplo-history/src/pages/Home.jsx
+++ front-end/secao-04/dia-02/exemplo-history/src/pages/Home.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 class Home extends React.Component {
     render() {
+
+        const { history } = this.props
         return (
-            <h1>Home Page</h1>
+            <>
+                <h1>Home Page</h1>
+                <button
+                    type='button'
+                    onClick={() => history.push('/welcome')}
+                >
+                    Acesse a página de boas vindas
+                </button>
+            </>
         );
     }
 }
--- front-end/secao-04/dia-02/exemplo-history/src/pages/Welcome.jsx
+++ front-end/secao-04/dia-02/exemplo-history/src/pages/Welcome.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Welcome extends React.Component {
     render() {
+        const { history } = this.props
         return (
-            <h1>Boas vindas</h1>
+            <>
+                <h1>Boas vindas</h1>
+                <button
+                    type='button'
+                    onClick={() => history.push('/')}
+                >
+                    Voltar
+                </button>
+            </>
         );
     }
 }
