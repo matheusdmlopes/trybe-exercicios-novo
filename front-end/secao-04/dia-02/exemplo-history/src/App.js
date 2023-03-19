4067b18cc81ce23f71712b43670981839554402f
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-03-19 16:27:09 -0300

    vou apagar tudo e refazer

--- front-end/secao-04/dia-02/exemplo-history/src/App.js
+++ front-end/secao-04/dia-02/exemplo-history/src/App.js
@@ -4,18 +4,17 @@ import Home from './pages/Home';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 
-class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/Welcome" component={Welcome} />
-        </Switch>
-      </BrowserRouter>
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/Welcome" component={Welcome} />
+      </Switch>
+    </BrowserRouter>
 
-    )
-  }
+  )
 }
+
 
 export default App;
