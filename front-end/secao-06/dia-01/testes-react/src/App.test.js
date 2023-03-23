3207ec3ce36e7132ae896aa55ef5a51c9db9f354
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-03-23 19:46:16 -0300

    testando o getbyrole e setar novas roles

--- front-end/secao-06/dia-01/testes-react/src/App.test.js
+++ front-end/secao-06/dia-01/testes-react/src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Verificando se existe o campo email', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  //acessando elementos da tela
+  const getInputByLabel = screen.getByLabelText('Email')
+  const getInputByRole = screen.getByRole('ab') // funciona mas nao é recomendado
+  const getInputByRole2 = screen.getByRole('textbox')
+  // ---------------------------------------
+  // const getInputByRole = screen.getByRole('input') 
+  // console.log(getInputByRole)
+
+  //interagindo com a aplicação
+
+  //testes
+  expect(getInputByLabel.type).toBe('email')
+  expect(getInputByLabel).toBeInTheDocument();
+
+
 });
