55b607c7e500c9e05a397d3cace43113c2d09616
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-03-23 23:35:18 -0300

    teste dando erro n sei pq

--- front-end/secao-06/dia-01/testes-react/src/App.js
+++ front-end/secao-06/dia-01/testes-react/src/App.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
+  state = {
+    email: '',
+    saveEmail: ''
+  }
+
+  handleChange = ({target}) => {
+    const {value, name} = target;
+
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  handleSaveEmail = () => {
+    const {email} = this.state;
+    this.setState({
+      saveEmail: email,
+      email: ''
+    })
+  }
+
   render() {
+    const {email, saveEmail} = this.state
   return (
     <div className="App">
       <label >
         Email
-        <input type="email" />
+        <input type="email" name='email' value={email} onChange={this.handleChange}/>
       </label>
-      <input type="button" value="Enviar" data-testid="botao-enviar" />
+      <input type="button" value="Enviar" data-testid="botao-enviar" onClick={this.handleSaveEmail} />
       <input type="button" value="Voltar" data-testid="botao-voltar" />
+      <h2 data-testid="id-email-user">Valor: {saveEmail} </h2>
     </div>
   );
 }
--- front-end/secao-06/dia-01/testes-react/src/App.test.js
+++ front-end/secao-06/dia-01/testes-react/src/App.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
 
 test('Verificando se existe o campo email', () => {
   render(<App />);
@@ -53,6 +55,29 @@ test('Verificando se existe um botão de enviar E um botão de voltar', () =>{
   expect(btnEnviar).toHaveValue('Enviar')
   expect(btnVoltar).toHaveValue('Voltar')
 
+})
+
+test('Testando se o botão e o campo email estão funcionando', () => {
+  render(<App />)
+  const EMAIL = 'teste@teste.com'
+
+  // ACESSANDO ELEMENTOS DA TELA
+  const getEmail = screen.getByLabelText('Email')
+  const getBtn = screen.getByTestId('botao-enviar')
+  const getTextEmail = screen.getByTestId('id-email-user')
+
+// INTERAGINDO COM A APLICAÇÃO
+  userEvent.type(getEmail, EMAIL)
+  userEvent.click(getBtn)
+
+// TESTES
+  expect(getEmail).toBeInTheDocument();
+  expect(getBtn).toBeInTheDocument();
+  expect(getTextEmail).toBeInTheDocument();
+
+  expect(getTextEmail).toHaveTextContent(`Valor: ${EMAIL}`)
+  expect(getEmail).toHaveValue('');
+  
 })
 
 // ACESSANDO ELEMENTOS DA TELA
