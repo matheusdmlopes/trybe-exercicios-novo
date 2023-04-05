b0d9b5a1b7c7881e997efe848a02acbabdbd411f
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-04-05 13:53:57 -0300

    fazendo o ex de fixaçao, ainda tá mt dificil isso to entendendo nd

--- front-end/secao-06/dia-02/teste-mock/src/App.test.js
+++ front-end/secao-06/dia-02/teste-mock/src/App.test.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
+
 
 afterEach(() => jest.clearAllMocks());
 
@@ -35,14 +37,32 @@ it('fetches a new joke when button is clicked', async () => {
   };
 
   jest.spyOn(global,'fetch');
-  global.fetch.mockResolvedValue({
+  global.fetch.mockResolvedValueOnce({
     json:jest.fn().mockResolvedValue(joke),
   })
 
   render(<App/>);
   
-  const getRenderedJoke=await screen.findByText('Whiteboards ... are remarkable.')
+  const getRenderedJoke= await screen.findByText('Whiteboards ... are remarkable.')
   expect(getRenderedJoke).toBeInTheDocument();
   expect(global.fetch).toHaveBeenCalledTimes(1)
   expect(global.fetch).toHaveBeenCalled();
+
+  const joke2 = {
+    id: '123',
+    joke: 'Sei lá.',
+    status: 369,
+  }
+  
+  const getBtn = screen.getByRole('button')
+
+  global.fetch.mockResolvedValueOnce({
+    json: jest.fn().mockResolvedValue(joke2)
+  })
+
+  userEvent.click(getBtn)
+
+  const getRenderedJoke2 = await screen.findByText('Sei lá.')
+  expect(getRenderedJoke2).toBeInTheDocument();
+  expect(screen.queryByText('Whiteboards ... are remarkable.')).not.toBeInTheDocument();
 });
