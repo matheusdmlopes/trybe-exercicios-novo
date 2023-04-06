53f0dba27f2094eead15b90e38aaade62992612a
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-04-06 14:08:13 -0300

    fazendo os testes

--- front-end/secao-06/dia-03/testes-router/src/App.test.js
+++ front-end/secao-06/dia-03/testes-router/src/App.test.js
@@ -1,8 +1,36 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 import App from './App';
+import renderWithRouter from './renderWithRouter';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('deve renderizar o componente App', () => {
+  renderWithRouter(<App />)
+
+  const title = screen.getByRole('heading', {
+    name: 'Você está na página Início',
+    level: 1
+  })
 });
+
+test('deve renderizar o componente sobre', () => {
+
+  const { history } = renderWithRouter(<App />)
+
+  const about = screen.getByRole('link', {
+    name: 'Sobre'
+  })
+  
+  act(() => {
+    userEvent.click(about)
+  })
+  
+
+  const {pathname} = history.location
+
+  expect(pathname).toBe('/about')
+
+  const aboutTitle = screen.getByRole('heading', {
+    name: 'Você está na página Sobre'
+  })
+})
