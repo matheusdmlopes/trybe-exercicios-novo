4870cca8e66a031bc25f7d26247e2bfea2589fa3
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-04-17 15:02:35 -0300

    exemplo feito

--- front-end/secao-07/dia-04/doguinhos-app/src/redux/indexStore.js
+++ front-end/secao-07/dia-04/doguinhos-app/src/redux/indexStore.js
@@ -1,7 +1,8 @@
-import { legacy_createStore as createStore } from "redux";
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import dogReducer from './reducers/dogReducer'
+import thunk from "redux-thunk";
 
-const store = createStore(dogReducer, composeWithDevTools())
+const store = createStore(dogReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store;
