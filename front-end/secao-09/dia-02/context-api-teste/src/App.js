387b8989f2dd09628abeab3c797423546d56ae0a
matheusdmlopes <matheusdemarcolopes@gmail.com>
2023-04-28 19:49:11 -0300

    começando dia 03

--- front-end/secao-09/dia-02/context-api-teste/src/App.js
+++ front-end/secao-09/dia-02/context-api-teste/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// /src/App.js
 
-function App() {
+import ThemeContext from './context/ThemeContext';
+import React from 'react';
+
+// ...
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{ color: 'dark' }}>
+      <div>
+        <Header />
+        <Image />
+        <Footer />
+      </div>
+    </ThemeContext.Provider>
   );
 }
-
-export default App;
---
+++ front-end/secao-09/dia-02/context-api-teste/src/context/ThemeContext.js
@@ -0,0 +1,6 @@
+// /src/context/ThemeContext.js
+import React, { createContext } from 'react';
+
+const ThemeContext = createContext();
+
+export default ThemeContext;
